7f82b739427f0180be07c5807078dc5b70e90743
ashim <ashimthapa664@gmail.com>
2022-10-21 09:13:33 +0545

    initial commit

--- app/src/main/java/com/example/practiceapp/DashActivity.kt
+++ app/src/main/java/com/example/practiceapp/DashActivity.kt
@@ -1,29 +1,96 @@
 package com.example.practiceapp
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.*
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import me.relex.circleindicator.CircleIndicator3
+import android.content.SharedPreferences;
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
 class DashActivity : AppCompatActivity() {
+
     private var titleList = mutableListOf<String>()
     private var descList = mutableListOf<String>()
     private lateinit var view_pager2: ViewPager2
+    lateinit var preferences: SharedPreferences
+    lateinit var progressbar: ProgressBar
+    var txtBatteryStatus: TextView? = null
+    var back: ImageView? = null;
+    var edit = preferences.edit()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash)
+        val name1: TextView = findViewById(R.id.tv_name)
+        var logoutbtn = findViewById<Button>(R.id.logoutbtn)
+        logoutbtn.setOnClickListener(){
+            edit.remove("full name")
+
+        }
+        preferences = getSharedPreferences(LandingActivity.FILE_NAME, Context.MODE_PRIVATE)
+        val name = preferences.getString("full name", "")
+        name1.text = name
+        progressbar = findViewById(R.id.progressbar)
+
         postToList()
+        txtBatteryStatus = findViewById(R.id.txtbattery)
+        registerReceiver(this.mBatteryInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        back?.setOnClickListener(View.OnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Please Confirm.")
+                .setMessage("You sure you want to Exit??")
+                .setPositiveButton("Yes") { _, _ ->
+                    finish()
+                }
+                .setNegativeButton("No", null)
+                .show()
+        })
     }
 
-    private fun addToList(title: String, description: String){
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(this.mBatteryInfoReceiver)
+    }
+
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "close", Toast.LENGTH_SHORT).show()
+        AlertDialog.Builder(this)
+            .setTitle("Please Confirm.")
+            .setMessage("You sure you want to Exit??")
+            .setPositiveButton("Yes") { _, _ ->
+                finish()
+            }
+            .setNegativeButton("No", null)
+            .show()
+    }
+
+    private val mBatteryInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val level = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, 101)
+
+            txtBatteryStatus!!.text = "Battery Status: $level%"
+            progressbar.progress = level
+
+        }
+    }
+
+    private fun addToList(title: String, description: String) {
         titleList.add(title)
         descList.add(description)
     }
 
-    private fun postToList(){
-        for (i in 1..5){
-            addToList("Title $i","Description $i")
+    private fun postToList() {
+        for (i in 1..5) {
+            addToList("Title $i", "Description $i")
         }
         view_pager2 = findViewById(R.id.view_pager2)
         view_pager2.adapter = ViewPagerAdapter(titleList, descList)
--- app/src/main/java/com/example/practiceapp/LandingActivity.kt
+++ app/src/main/java/com/example/practiceapp/LandingActivity.kt
@@ -1,16 +1,19 @@
 package com.example.practiceapp
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
@@ -18,11 +21,10 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class LandingActivity : AppCompatActivity() {
+    lateinit var sharedPreferences: SharedPreferences
 
     companion object {
-        public lateinit var email: EditText
-        public lateinit var pass: EditText
-        public lateinit var confirmpass: EditText
+        var FILE_NAME: String = "shared_pref"
     }
 
     private lateinit var btnsignup: Button
@@ -32,11 +34,22 @@ class LandingActivity : AppCompatActivity() {
     private lateinit var signup: Button
     private lateinit var signin: Button
     private lateinit var firebaseAuth: FirebaseAuth
-
+    private lateinit var email: EditText
+    private lateinit var pass: EditText
+    private lateinit var confirmpass: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        if (sharedPreferences.getString("full name", "") != "") {
+            val intent = Intent(this, DashActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
         setContentView(R.layout.activity_landing)
+
         firebaseAuth = FirebaseAuth.getInstance()
 
         // SIGN_UP
@@ -51,8 +64,17 @@ class LandingActivity : AppCompatActivity() {
             signup.setOnClickListener() {
                 signupActivity()
             }
-
         }
+
+
+        var btnsignup1 = findViewById<Button>(R.id.btnSignUp)
+        sharedPreferences = getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+        btnsignup1.setOnClickListener() {
+            val intent = Intent(this, DashActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
         closebtnsignup.setOnClickListener() {
             btmshtsignup.state = BottomSheetBehavior.STATE_COLLAPSED
@@ -60,13 +82,11 @@ class LandingActivity : AppCompatActivity() {
 
 
         // SIGN_IN
-        //    var btmshtsignin2 = BottomSheetBehavior.from(findViewById(R.id.btmsheetsignin))
         btmshtsignin.setPeekHeight(0)
         btnsignin = findViewById(R.id.landingbtnsignin)
         btnsignin.setOnClickListener() {
-        //   signinActivity()
-            val intent = Intent(this, DashActivity::class.java)
-            startActivity(intent)
+            signinActivity()
+
         }
         closebtnsignin = findViewById(R.id.closebtnsignin)
         closebtnsignin.setOnClickListener() {
@@ -85,26 +105,21 @@ class LandingActivity : AppCompatActivity() {
         val email: String = email.text.toString()
         val pass: String = pass.text.toString()
         val confirmpass: String = confirmpass.text.toString()
-        Log.i("as","asdf")
         if (email.isNotEmpty() && pass.isNotEmpty() && confirmpass.isNotEmpty()) {
             if (pass == confirmpass) {
                 firebaseAuth.createUserWithEmailAndPassword(email, pass)
-                    .addOnFailureListener{
-                        Toast.makeText(this,""+it.message,Toast.LENGTH_SHORT).show()
+                    .addOnFailureListener {
+                        Toast.makeText(this, "" + it.message, Toast.LENGTH_SHORT).show()
                     }
-                    .addOnCompleteListener {
-                        Log.i("ashim","Here")
-                        if (it.isSuccessful) {
-                            btmshtsignup.state = BottomSheetBehavior.STATE_COLLAPSED
-                            btmshtsignin.state = BottomSheetBehavior.STATE_EXPANDED
-                        } else {
-                            Toast.makeText(
-                                this,
-                                it.exception.toString(),
-                                Toast.LENGTH_SHORT
-                            )
-                                .show()
-                        }
+                    .addOnSuccessListener {
+                        btmshtsignup.state = BottomSheetBehavior.STATE_COLLAPSED
+                        btmshtsignin.state = BottomSheetBehavior.STATE_EXPANDED
+                        val nameId = findViewById<TextInputEditText>(R.id.etFullName)
+                        val name: String = nameId.text.toString()
+                        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+                        editor.putString("full name", name)
+                        editor.apply()
+
                     }
 
             } else {
@@ -127,11 +142,10 @@ class LandingActivity : AppCompatActivity() {
             val email: String = email.text.toString()
             val pass: String = pass.text.toString()
             if (email.isNotEmpty() && pass.isNotEmpty()) {
-                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnFailureListener{
-                    Toast.makeText(this,""+it.message,Toast.LENGTH_SHORT).show()
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnFailureListener {
+                    Toast.makeText(this, "" + it.message, Toast.LENGTH_SHORT).show()
                 }.addOnCompleteListener {
                     if (it.isSuccessful) {
-                        //     btmshtsignin2.state = BottomSheetBehavior.STATE_COLLAPSED
                         val intent = Intent(this, DashActivity::class.java)
                         startActivity(intent)
                         finish()
@@ -147,9 +161,7 @@ class LandingActivity : AppCompatActivity() {
         }
 
 
-
-
-        }
+    }
 
 }
 
